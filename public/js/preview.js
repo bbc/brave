fb9421dcd2542908c040216ed9e4d9e0bad2c09f
Luke Moscrop <luke.moscrop@bbc.co.uk>
2018-11-07 12:38:49 +0000

    WebRTC audio levels
    Adds support for showing the audio levels reported from gstreamer level element

--- public/js/preview.js
+++ public/js/preview.js
@@ -110,6 +110,104 @@ preview._handlePreviewRequest = function(request) {
     }
 }
 
+preview._clamp = function(value, min = 0, max = 1) {
+    return Math.min(Math.max(value, min), max);
+}
+
+preview._normaliseDb = function(db) {
+    //  -60db -> 1.00 (very quiet)
+    //  -30db -> 0.75
+    //  -15db -> 0.50
+    //  -5db -> 0.25
+    //  -0db -> 0.00 (very loud)
+    var logscale = 1 - Math.log10(-0.15 * db + 1);
+    return preview._clamp(logscale);
+}
+
+preview._gradient = function(context, brightness, darkness, height) {
+    var gradient = context.createLinearGradient(0, 0, 0, height);
+
+    gradient.addColorStop(0.0, `rgb(${brightness}, ${darkness}, ${darkness})`);
+    gradient.addColorStop(0.22, `rgb(${brightness}, ${brightness}, ${darkness})`);
+    gradient.addColorStop(0.25, `rgb(${brightness}, ${brightness}, ${darkness})`);
+    gradient.addColorStop(0.35, `rgb(${darkness}, ${brightness}, ${darkness})`);
+    gradient.addColorStop(1.0, `rgb(${darkness}, ${brightness}, ${darkness})`);
+
+    return gradient;
+}
+
+preview.updateAudioLevels = function() {
+    var context = document.getElementById("audio_levels").getContext("2d");
+    var channels = websocket.volume.channels;
+    var channel;
+    var margin = 2;
+    var width = 80;
+    var height = 360;
+    var channelWidth = parseInt((width - (margin * (channels - 1))) / channels);
+    var peak;
+
+    //console.log(`width: ${width} filled with ${channels} channels of each ${channelWidth} and ${channels - 1} margin of ${margin}`);
+
+    var bgFill = preview._gradient(context, 64, 0, height);
+    var rmsFill =  preview._gradient(context, 255, 0, height);
+    var peakFill = preview._gradient(context,  192, 0, height);
+    var decayFill = preview._gradient(context,  255, 127, height);
+
+    //Clear the canvas
+    context.clearRect(0,0, width, height);
+
+    for (channel = 0; channel < channels; channel++) {
+        var audioData = JSON.parse(websocket.volume.data[channel]);
+
+        var rms = preview._normaliseDb(audioData.rms) * height;
+        peak = preview._normaliseDb(audioData.peak) * height;
+        var decay = preview._normaliseDb(audioData.decay) * height;
+
+        var x = (channel * channelWidth) + (channel * margin);
+
+        //draw background
+        context.fillStyle = bgFill;
+        context.fillRect(x, 0, channelWidth, height - peak);
+
+        // draw peak bar
+        context.fillStyle = peakFill;
+        context.fillRect(x, height - peak, channelWidth, peak);
+
+        // draw rms bar below
+        context.fillStyle = rmsFill;
+        context.fillRect(x, height - rms, channelWidth, rms - peak);
+
+        // draw decay bar
+        context.fillStyle = decayFill;
+        context.fillRect(x, height - decay, channelWidth, 2);
+
+        // draw medium grey margin bar
+        context.fillStyle = "gray";
+        context.fillRect(x + channelWidth, 0, margin, height);
+    }
+
+    context.fillStyle= "white"
+    context.font = "10px Arial";
+
+    var dbMarkers = [-40, -20, -10, -5, -4, -3, -2, -1];
+    dbMarkers.forEach(function(db) {
+        var text = db.toString();
+        var y = preview._normaliseDb(db) * height;
+        var textwidth = context.measureText(text).width;
+        var textheight = 10;
+
+        if (y > peak) {
+            context.fillStyle= "white";
+        } else {
+            context.fillStyle= "black";
+        }
+ 
+        context.fillText(text, (width - textwidth) - 2, height - y - textheight);
+    });
+
+    window.requestAnimationFrame( preview.updateAudioLevels );
+}
+
 preview.setVideoSrc = function(src) {
     preview._createVideoPlayer()
     getVideoElement().srcObject = src;
@@ -127,9 +225,18 @@ preview._createVideoPlayer = function() {
     var video = document.createElement('video')
     video.id = 'stream'
     video.autoplay = true
+
+    var audio_canvas = document.createElement('canvas')
+    audio_canvas.id = 'audio_levels';
+    audio_canvas.width  = 80;
+    audio_canvas.height = 360;
+
     $('#preview-bar').empty()
     $('#preview-bar').append(video)
+    $('#preview-bar').append(audio_canvas)
+
     preview._showMuteButton()
+    preview.updateAudioLevels();
 }
 
 preview._createImage = function() {
