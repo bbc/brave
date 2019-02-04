e3d70680575702c3e1ad5095c5af8afd1050405e
Matthew Clark <matthew1000@gmail.com>
2019-02-04 14:21:04 +0000

    Update test to show zorders and mixers

--- tests/test_mixer_to_mixer.py
+++ tests/test_mixer_to_mixer.py
@@ -2,33 +2,73 @@ import time, pytest, inspect
 from utils import *
 from PIL import Image
 
+GREEN=[0, 255, 0]
+BLUE=[0, 0, 255]
+
 
 def test_mixer_to_mixer(run_brave, create_config_file):
     '''Ensure a mixer can accept another mixer as source'''
-    subtest_start_brave_with_two_mixers(run_brave, create_config_file)
-    subtest_add_mixer1_as_source_of_mixer2()
+    subtest_start_brave_with_three_mixers(run_brave, create_config_file)
+    subtest_add_mixer2_as_source_of_mixer1()
 
     # mixer1's background is red.
     # mixer2's background is green.
-    # mixer1 has been added as a source of mixer1, so the output of mixer2 should be red.
-    subtest_ensure_mixer2_is_red()
+    # mixer2 has been added as a source of mixer1, so the output of mixer1 should be green.
+    subtest_ensure_mixer1_color(GREEN)
 
+    subtest_add_mixer3_as_source_of_mixer1()
 
-def subtest_add_mixer1_as_source_of_mixer2():
-    cut_to_source('mixer1', 2)
+    # mixer3 had a lower zorder than mixer2 so it will still be the color of mixer2 (green_)
+    subtest_ensure_mixer1_color(GREEN)
+
+    # But if we increase the zorder of mixer3, then it will appear on top of mixer2, making it blue
+    subtest_update_mixer3_zorder()
+    subtest_ensure_mixer1_color(BLUE)
+
+def subtest_add_mixer2_as_source_of_mixer1():
+    overlay_source('mixer2', 1, details={'zorder': 2})
     time.sleep(1)
     assert_everything_in_playing_state()
 
 
-def subtest_ensure_mixer2_is_red():
-    add_output({'type': 'image', 'source': 'mixer2'})
+def subtest_add_mixer3_as_source_of_mixer1():
+    overlay_source('mixer3', 1, details={'zorder': 1})
+    time.sleep(1)
+    assert_everything_in_playing_state()
+    assert_mixers([
+        {'pattern': 4, 'sources': [
+            {'uid': 'mixer2', 'in_mix': True, 'zorder': 2},
+            {'uid': 'mixer3', 'in_mix': True, 'zorder': 1}
+        ]},
+        {'pattern': 5, 'sources': []},
+        {'pattern': 6, 'sources': []}
+    ])
+
+def subtest_ensure_mixer1_color(color):
     time.sleep(3)
-    assert_image_output_color(1, [255, 0, 0])
+    assert_image_output_color(1, color)
 
 
-def subtest_start_brave_with_two_mixers(run_brave, create_config_file):
-    # Pattern 4 is red and pattern 5 is green
-    config = {'mixers': [{'pattern': 4}, {'pattern': 5}]}
+def subtest_update_mixer3_zorder():
+    overlay_source('mixer3', 1, details={'zorder': 3})
+    time.sleep(1)
+    assert_everything_in_playing_state()
+    assert_mixers([
+        {'pattern': 4, 'sources': [
+            {'uid': 'mixer2', 'in_mix': True, 'zorder': 2},
+            {'uid': 'mixer3', 'in_mix': True, 'zorder': 3}
+        ]},
+        {'pattern': 5, 'sources': []},
+        {'pattern': 6, 'sources': []}
+    ])
+
+
+def subtest_start_brave_with_three_mixers(run_brave, create_config_file):
+    # Pattern 4 is red, pattern 5 is green, pattern 6 is blue
+    config = {
+        'mixers': [{'pattern': 4}, {'pattern': 5}, {'pattern': 6}],
+        'outputs': [{'type': 'image', 'source': 'mixer1'}]
+    }
     config_file = create_config_file(config)
     run_brave(config_file.name)
     time.sleep(0.5)
